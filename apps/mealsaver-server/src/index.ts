a89807d7af55b16052966c7f67bcdb7991409efe
Paul Szilveszter <szilveszter_paul@yahoo.com>
2023-05-14 20:24:04 +0300

    Implemented JWT

--- apps/mealsaver-server/src/index.ts
+++ apps/mealsaver-server/src/index.ts
@@ -1,93 +1,120 @@
-import express, { Request, Response, Express } from "express"
-import bcrypt from "bcrypt"
+/* eslint-disable turbo/no-undeclared-env-vars */
+import express, { Request, Response, Express } from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv"
+
+import { expireTime } from "./constants";
+
+dotenv.config()
+
+const SECRET_KEY = process.env.SECRET_KEY; 
+const REFRESH_SECRET_KEY = process.env.REFRESH_SECRET_KEY;
 
 
 
-interface UserData  {
-    username: string;
-    email:string;
-    password:string;
+if (!SECRET_KEY || !REFRESH_SECRET_KEY) {
+  console.error("Secret keys are not defined in environment variables");
+  process.exit(1);
+}
+
+interface UserData {
+  username: string;
+  email: string;
+  password: string;
 }
 
 
-const SERVER: Express = express()
-SERVER.use(express.json())
+const SERVER: Express = express();
+SERVER.use(express.json());
+
+const users: Array<UserData> = new Array<UserData>();
+let refreshTokens: Array<string> = [];
 
 
 
+const authenticateToken = (req: Request, res: Response, next: any) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
+  if (token == null) return res.sendStatus(401);
 
+  jwt.verify(token, SECRET_KEY, (err: any, user: any) => {
+    if (err) return res.sendStatus(403);
+    req.body.user = user;
+    next();
+  });
+};
 
+SERVER.post("/users/register", async (req: Request, res: Response) => {
+  try {
+    const username: string = req.body.username;
+    const email: string = req.body.email;
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const user: UserData = { username: username, email: email, password: hashedPassword };
+    users.push(user);
+    res.status(201).send();
+  } catch {
+    res.status(500).send();
+  }
+});
 
+SERVER.post("/users/login", async (req: Request, res: Response) => {
+  const user: UserData | undefined = users.find((user: UserData) => user.username == req.body.username);
 
+  if (user == undefined) {
+    return res.status(400).send("Cannot find the user");
+  }
 
-
-
-const users: Array<UserData> = new Array<UserData>
-
-
-
-SERVER.get('/users', (req: Request, res: Response) => {
-    res.json(users)
-})
-
-
-
-
-
-
-SERVER.post('/users', async (req: Request, res: Response) => {
-
-    try {
-        
-        const username:string = req.body.username;
-        const email:string = req.body.email;
-        
-        // const salt = await bcrypt.genSalt()
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        // console.log(salt)
-        // console.log(hashedPassword)
-
-
-
-
-        const user:UserData = { username: username, email:email, password: hashedPassword }
-
-        users.push(user)
-
-        res.status(201).send()
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      const accessToken = jwt.sign(user, SECRET_KEY, { expiresIn: expireTime });
+      const refreshToken = jwt.sign(user, REFRESH_SECRET_KEY);
+      refreshTokens.push(refreshToken);
+      res.json({ accessToken: accessToken, refreshToken: refreshToken });
+    } else {
+      res.send("Failed Login");
     }
-    catch {
-        res.status(500).send()
-    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).send();
+  }
+});
 
+SERVER.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, REFRESH_SECRET_KEY, (err: any, user: any) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = jwt.sign({ username: user.username, email: user.email }, SECRET_KEY, { expiresIn: '15m' });
+    res.json({ accessToken: accessToken });
+  });
+}); 
 
+SERVER.delete("/logout", (req, res) => {
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token);
+  res.sendStatus(204);
+});
 
-})
+SERVER.put("/users/me", authenticateToken, async (req: Request, res: Response) => {
+  // Verify that the username in the token matches the username in the request body
+  if (req.body.user.username !== req.body.username) {
+    return res.status(403).send("You can only update your own data");
+  }
 
-
-SERVER.post('/users/login', async(req: Request, res: Response) => {
-    const user:UserData | undefined = users.find((user:UserData)=> user.username == req.body.username)
-
-    if(user == undefined){
-        return res.status(400).send('Cannot find the user')
-    }
-
-    try{
-
-      if(await bcrypt.compare(req.body.password, user.password)){
-        res.send('Succes')
+  // Update the user's data
+  const user = users.find((user) => user.username === req.body.username);
+  if (user) {
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+        user.password = await bcrypt.hash(req.body.password, 10);
       }
-      else{
-        res.send('Failed Login')
-      }
+      res.status(200).send("User data updated");
+    } else {
+      res.status(404).send("User not found");
     }
-    catch(e){
-        console.error(e)
-
-        res.status(500).send()
-    }
-})
-
-
-SERVER.listen(3000)
+  });
+  
+  SERVER.listen(3000);
+  
